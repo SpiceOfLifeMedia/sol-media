170eca5b405f954edc65cbdbff4b16031d5a701a
SpiceOfLifeMedia <info@spiceoflifemedia.com.au>
2026-05-20 11:21:45 +0930

    Resize favicon to spec 512x512; reorder Home so Reviews (trust) lands above Services

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,8 +13,8 @@ export default function Home() {
     <Layout>
       <Hero />
       <TrustStrip />
-      <Services />
       <Reviews />
+      <Services />
       <SelectedWork />
       <Process />
       <FAQ />
