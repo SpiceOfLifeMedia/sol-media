3d56cc364f71518f6c2491235abf7aa4599e5739
SpiceOfLifeMedia <info@spiceoflifemedia.com.au>
2026-09-05 11:17:42 +0930

    Retire website launch special
    Refs #10

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,8 @@ import InsightArticle from '@/pages/InsightArticle';
 import Work from '@/pages/Work';
 import FullCircleCaseStudy from '@/pages/FullCircleCaseStudy';
 import HillierCaseStudy from '@/pages/HillierCaseStudy';
+import PetiolaWilsonCaseStudy from '@/pages/PetiolaWilsonCaseStudy';
 import Order from '@/pages/Order';
-import LandingPageSprint from '@/pages/LandingPageSprint';
-import WebsiteSprintTerms from '@/pages/WebsiteSprintTerms';
 
 import DigitalPresenceAudit from '@/pages/services/DigitalPresenceAudit';
 import BrandSystems from '@/pages/services/BrandSystems';
@@ -59,16 +58,6 @@ function EtsyCustomCdOrder() {
   return <Order />;
 }
 
-function LegacyWebSprintRedirect() {
-  const [, navigate] = useLocation();
-
-  useEffect(() => {
-    navigate('/website-launch-special', { replace: true });
-  }, [navigate]);
-
-  return null;
-}
-
 function Router() {
   return (
     <Switch>
@@ -78,14 +67,11 @@ function Router() {
       <Route path="/agency" component={Agency} />
       <Route path="/privacy" component={Privacy} />
       <Route path="/start-a-project" component={StartAProject} />
-      <Route path="/website-launch-special" component={LandingPageSprint} />
-      <Route path="/website-sprint" component={LegacyWebSprintRedirect} />
-      <Route path="/website-sprint-terms" component={WebsiteSprintTerms} />
-      <Route path="/websprint" component={LegacyWebSprintRedirect} />
       <Route path="/insights" component={Insights} />
       <Route path="/work" component={Work} />
       <Route path="/work/full-circle-hair-society" component={FullCircleCaseStudy} />
       <Route path="/work/hillier-plumbing-excavation" component={HillierCaseStudy} />
+      <Route path="/work/petiola-wilson" component={PetiolaWilsonCaseStudy} />
       <Route path="/custom-cds" component={DirectCustomCdOrder} />
       <Route path="/custom-cd-order" component={EtsyCustomCdOrder} />
       <Route path="/order" component={LegacyOrderRedirect} />
